81af2161007e1c1dba4d28ca3cf3db1ce383a0e4
Naoki Matagawa <naoki.m@asial.co.jp>
2017-03-05 18:49:13 +0900

    Remove subcommand `core`.

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const program = require('commander');
-const core = require('./subcommands/core.js');
 
 program
   .version(require('./package.json').version)
-
-program
-  .command('core')
-  .action(core);
 
 program.parse(process.argv);
