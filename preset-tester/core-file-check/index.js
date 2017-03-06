e00e56b44d65d006b145885609e24550efa28934
Naoki Matagawa <naoki.m@asial.co.jp>
2017-03-06 14:39:39 +0900

    feat(run): Add preset tester `core-file-check` with dummy behavior.

---
+++ preset-tester/core-file-check/index.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const fs = require('fs');
+
+module.exports = (input) => {
+  {// Check if or not package exists
+    const p = path.resolve(input.targetOutDir, 'npm-packages', 'onsenui', input.target.core);
+
+    console.log(`${JSON.stringify(input.target)} - ${input.tester.id} - ${input.testcase.id} - ${fs.existsSync(p) ? 'package exists' : 'package does not exist'}`);
+  }
+}
