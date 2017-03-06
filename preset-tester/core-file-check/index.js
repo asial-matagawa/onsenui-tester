2544d8c67d671141cb2e2f144519bb4d66e868a1
Naoki Matagawa <naoki.m@asial.co.jp>
2017-03-06 14:41:26 +0900

    chore(*): Rename `npm-packages` with `npm-package`.

--- preset-tester/core-file-check/index.js
+++ preset-tester/core-file-check/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 module.exports = (input) => {
   {// Check if or not package exists
-    const p = path.resolve(input.targetOutDir, 'npm-packages', 'onsenui', input.target.core);
+    const p = path.resolve(input.targetOutDir, 'npm-package', 'onsenui', input.target.core);
 
     console.log(`${JSON.stringify(input.target)} - ${input.tester.id} - ${input.testcase.id} - ${fs.existsSync(p) ? 'package exists' : 'package does not exist'}`);
   }
