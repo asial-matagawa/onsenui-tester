d9d62164d920a75c56513b638bff2f72df4c0c05
Naoki Matagawa <naoki.m@asial.co.jp>
2017-03-06 19:41:18 +0900

    feat(tester/core-file-check): Implement with dummy data.

--- preset-tester/core-file-check/index.js
+++ preset-tester/core-file-check/index.js
@@ -1,10 +1,17 @@
 const path = require('path');
 const fs = require('fs');
+const chalk = require('chalk');
 
 module.exports = (input) => {
   {// Check if or not package exists
-    const p = path.resolve(input.targetCacheDir, 'npm-package', 'onsenui', input.target.core);
+    const onsenuiPath = path.resolve(input.targetCacheDir, 'npm-package', 'onsenui', input.target.core);
 
-    console.log(`${JSON.stringify(input.target)} - ${input.tester.id} - ${input.testcase.id} - ${fs.existsSync(p) ? 'package exists' : 'package does not exist'}`);
+    if (!fs.existsSync(onsenuiPath)) {
+      throw new Error('`onsenui` package is not cached.');
+    }
+
+    for (const filePath of ['js/onsenui.js', 'js/angular-onsenui.js']) {
+      console.log(`${chalk.magenta(filePath)}: ${fs.existsSync(path.resolve(onsenuiPath, filePath)) ? chalk.green('exists') : chalk.red('does not exist')}`);
+    }
   }
 }
