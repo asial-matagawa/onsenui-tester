53f30bfb0f894f726bbd2b32f48f0bb6b1f24913
Naoki Matagawa <naoki.m@asial.co.jp>
2017-03-06 19:51:42 +0900

    feat(tester/core-file-check): Change dummy data.

--- preset-tester/core-file-check/index.js
+++ preset-tester/core-file-check/index.js
@@ -10,7 +10,27 @@ module.exports = (input) => {
       throw new Error('`onsenui` package is not cached.');
     }
 
-    for (const filePath of ['js/onsenui.js', 'js/angular-onsenui.js']) {
+    const filePaths = [
+      '.npmignore',
+      'CHANGELOG.md',
+      'LICENSE',
+      'README.md',
+      'bower.json',
+      'css/onsenui.css',
+      'css/onsen-css-components.css',
+      'css/onsen-css-components-default.css',
+      'css/ionicons',
+      'css/font_awesome',
+      'css/material-design-iconic-font',
+      'js/onsenui.js',
+      'js/onsenui.min.js',
+      'js/onsenui.d.ts',
+      'js/angular-onsenui.js',
+      'js/angular-onsenui.min.js',
+      'package.json',
+      'stylus',
+    ];
+    for (const filePath of filePaths) {
       console.log(`${chalk.magenta(filePath)}: ${fs.existsSync(path.resolve(onsenuiPath, filePath)) ? chalk.green('exists') : chalk.red('does not exist')}`);
     }
   }
